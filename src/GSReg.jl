cad6784e3feed01a84b36ceac9737e3b15b6adeb
adanmauri <adan.mauri@gmail.com>
2018-03-29 19:58:42 -0300

    noconstant parameter defaut to false. nocontant and varnames change to positional parameters

--- src/GSReg.jl
+++ src/GSReg.jl
@@ -2,17 +2,19 @@ module GSReg
 
 using DataFrames
 
-function gsreg(depvar::Array, indepvars::Array, noconstant::Bool=true, varnames::Array=[])
-    # TODO: Implement gsreg
+NOCONSTANT_DEFAULT = false
+VARNAMES_DEFAULT = []
 
+function gsreg(depvar::Array, indepvars::Array; noconstant::Bool=NOCONSTANT_DEFAULT, varnames::Array=VARNAMES_DEFAULT)
+    # TODO: Implement gsreg
     return true
 end
 
-function gsreg(equation::String, data::DataFrame, noconstant::Bool=true)
-    return GSReg.gsreg([String(ss) for ss in split(replace(equation, r"\s+|\s+$/g", ","), ",")], data, noconstant)
+function gsreg(equation::String, data::DataFrame; noconstant::Bool=NOCONSTANT_DEFAULT)
+    return GSReg.gsreg([String(ss) for ss in split(replace(equation, r"\s+|\s+$/g", ","), ",")], data, noconstant=noconstant)
 end
 
-function gsreg(equation::Array{String}, data::DataFrame, noconstant::Bool=true)
+function gsreg(equation::Array{String}, data::DataFrame; noconstant::Bool=NOCONSTANT_DEFAULT)
     keys = names(data)
     n_equation = []
     for e in equation
@@ -22,14 +24,14 @@ function gsreg(equation::Array{String}, data::DataFrame, noconstant::Bool=true)
             append!(n_equation, [e])
         end
     end
-    return GSReg.gsreg(map(Symbol, unique(n_equation)), data, noconstant)
+    return GSReg.gsreg(map(Symbol, unique(n_equation)), data, noconstant=noconstant)
 end
 
-function gsreg(equation::Array{Symbol}, data::DataFrame, noconstant::Bool=true)
+function gsreg(equation::Array{Symbol}, data::DataFrame; noconstant::Bool=NOCONSTANT_DEFAULT)
     varnames = map(string, data.colindex.names)
     depvar = Array{Float64}(data[1:end, 1])
     indepvars = Array{Float64}(data[1:end, equation])
-    return GSReg.gsreg(depvar, indepvars, noconstant, varnames)
+    return GSReg.gsreg(depvar, indepvars, noconstant=noconstant, varnames=varnames)
 end
 
 end
