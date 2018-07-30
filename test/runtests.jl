cb8338d1799937114f94d65fbba2042b556b2389
Valentin Mari <valen.mari@ubykuo.com>
2018-07-30 01:14:53 -0300

    Added basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
-using GSReg
-using Base.Test
+using GSReg, Base.Test, DataFrames, CSV, Distributions
+data = DataFrame(randn(10,6))
 
-# write your own tests here
-# we promess a lot of test to show the real power of this library
-@test 1 == 1
+res = gsreg("x1 x2 x3 x4 x5 x6", data; ttest=true, residualtest=true, criteria=[:aic, :bic], csv="pepe.csv")
+@test size(res.results,1) == 31
