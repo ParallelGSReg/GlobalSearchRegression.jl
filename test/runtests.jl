6660250dbe78db984c437776403ebd014a9f7642
Valentin Mari <valen.mari@ubykuo.com>
2018-09-27 00:21:17 -0300

    Fix build

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
-Pkg.add("Test")
-using GSReg, Test, DataFrames, CSV, Distributions
+using GSReg, Test, DataFrames, Distributions
 data = DataFrame(randn(10,6))
 
 res = gsreg("x1 x2 x3 x4 x5 x6", data; ttest=true, residualtest=true, criteria=[:aic, :bic], csv="pepe.csv")
