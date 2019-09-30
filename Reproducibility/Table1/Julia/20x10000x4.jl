d483357469be19a555513d520d702f12df971146
nicomzn <52471741+nicomzn@users.noreply.github.com>
2019-09-30 17:32:50 -0300

    Add files via upload

---
+++ Reproducibility/Table1/Julia/20x10000x4.jl
@@ -0,0 +1,11 @@
+using GlobalSearchRegression, DataFrames, Distributions, CSV
+# Insert here your working directory path
+data = DataFrame(Array{Union{Missing,Float64}}(randn(10000,21)))
+headers = [ :y ; [ Symbol("x$i") for i = 1:size(data,2) - 1 ] ]
+names!(data, headers )
+b=@elapsed gsreg("y x*", data; ttest=true, criteria=[:aic], parallel=4, csv="20x10000x4.csv")
+t=DataFrame(A=Float64[])
+push!(t,b)
+CSV.write("juliatimmings.csv", t; append=true)
+exit()
+
