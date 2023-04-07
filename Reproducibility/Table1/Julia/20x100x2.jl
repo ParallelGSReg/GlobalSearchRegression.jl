8dbddda21c40f29c6623b8f5f8e62e3ea57cfabe
Demian Panigo <panigo@gmail.com>
2023-04-06 21:37:02 -0300

    reproducibility update

---
+++ Reproducibility/Table1/Julia/20x100x2.jl
@@ -0,0 +1,11 @@
+using GlobalSearchRegression, DataFrames, Distributions, CSV
+# Insert here your working directory path
+data = DataFrame(Array{Union{Missing,Float64}}(randn(100,21), :auto))
+headers = [ :y ; [ Symbol("x$i") for i = 1:size(data,2) - 1 ] ]
+rename!(data, headers)
+b=@elapsed gsreg("y x*", data; ttest=true, criteria=[:aic], parallel=2, csv="20x100x2.csv")
+t=DataFrame(A=Float64[0.0])
+t[1,1]=b
+CSV.write("juliatimmings.csv", t; append=true)
+exit()
+
--- Reproducibility/Table1/Julia/25x1000x4.jl
+++ Reproducibility/Table1/Julia/25x1000x4.jl
@@ -1,11 +1,11 @@
 using GlobalSearchRegression, DataFrames, Distributions, CSV
 # Insert here your working directory path
-data = DataFrame(Array{Union{Missing,Float64}}(randn(1000,26)))
+data = DataFrame(Array{Union{Missing,Float64}}(randn(1000,26)), :auto)
 headers = [ :y ; [ Symbol("x$i") for i = 1:size(data,2) - 1 ] ]
-names!(data, headers )
+rename!(data, headers)
 b=@elapsed gsreg("y x*", data; ttest=true, criteria=[:aic], parallel=4, csv="25x1000x4.csv")
-t=DataFrame(A=Float64[])
-push!(t,b)
+t=DataFrame(A=Float64[0.0])
+t[1,1]=b
 CSV.write("juliatimmings.csv", t; append=true)
 exit()
 
